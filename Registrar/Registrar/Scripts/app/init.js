5dd1a7205dc8f80dfc0273ec49cce37ec4689551
Matthew Lee <mslee61@gmail.com>
2014-02-19 10:55:20 -0600

    Stripped everything

--- Registrar/Registrar/Scripts/app/init.js
+++
@@ -1,8 +0,0 @@
-﻿registra.r("init.app", function () {
-    (function ($, ko, exports) {
-        //set up object for the rest of the app to use; 
-        exports.app = {
-            services: {}
-        };
-    })(window.jQuery, window.ko, window);
-}).OnReady(["init.app", "services.app", "viewmodel.app", "bindings.app"]);
---
+++ sample/viewModel.js
@@ -0,0 +1,12 @@
+﻿registra.r("viewmodel.app", function () {
+    (function ($, ko, exports, services) {
+        function ViewModel() {
+            var self = this;
+
+            self.id = ko.observable();
+            self.name = ko.observable("testing");
+            self.description = ko.observable("description);
+        }
+        exports.ViewModel = ViewModel;
+    })(window.jQuery, window.ko, window.app, window.services);
+});
